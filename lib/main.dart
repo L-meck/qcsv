076d1e24d999f34d4b6cabe6d0db79e88ef2e79d
L-meck <Kanaidavid3@gmail.com>
2022-10-13 11:58:55 +0300

    bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:csv/csv.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+import 'data.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -49,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
   //   selectedItem = _data[4] as String;
   // }
 /////////////////////////////////
-    int _currentStep = 0;
+  int _currentStep = 0;
 
   _stepState(int step) {
     if (_currentStep > step) {
@@ -61,20 +63,117 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _steps() => [
         Step(
-          title: const Text('Address'),
-          content: const _AddressForm(),
+          title: const Text('Choose CSV File:'),
+          content: GestureDetector(
+            onTap: () => _pickFile(),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: _fileName == null ? Colors.blueGrey : Colors.green,
+                  borderRadius: const BorderRadius.all(Radius.circular(5))),
+              height: 100,
+              child: Row(
+                children: [
+                  _fileName == null
+                      ? Center(
+                          child: Row(
+                            children: const [
+                              Padding(
+                                padding:
+                                    EdgeInsets.only(left: 20.0, right: 8.0),
+                                child: Icon(
+                                  Icons.coronavirus_sharp,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                'Tap to select a your file',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.only(left: 20.0, right: 8.0),
+                              child: Icon(
+                                Icons.tips_and_updates_sharp,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              _fileName.toString(),
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
+                ],
+              ),
+            ),
+          ),
           state: _stepState(0),
           isActive: _currentStep == 0,
         ),
         Step(
-          title: const Text('Card Details'),
-          content: const _CardForm(),
+          title: const Text('Choose your name:'),
+          content: Column(
+            children: [
+              DropdownButton(
+                icon: const Icon(Icons.keyboard_arrow_down),
+                items: _data.map((item) {
+                  return DropdownMenuItem(
+                    value: selectedItem,
+                    child: GestureDetector(
+                      child: Text(
+                        item[4].toString(),
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _tsrName = item[4].toString();
+                        });
+                      },
+                    ),
+                  );
+                }).toList(),
+                onChanged: (String? val) {
+                  setState(() {
+                    selectedItem = _tsrName.toString();
+                  });
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Container(
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                      color: _tsrName == null ? Colors.blueGrey : Colors.green,
+                      borderRadius: const BorderRadius.all(Radius.circular(5))),
+                  child: Center(
+                    child: Text(
+                      _tsrName.toString(),
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
           state: _stepState(1),
           isActive: _currentStep == 1,
         ),
         Step(
-          title: const Text('Overview'),
-          content: const _Overview(),
+          title: const Text('Check your Warnings & Stats:'),
+          content: Column(
+            children: const [],
+          ),
           state: _stepState(2),
         )
       ];
@@ -95,163 +194,81 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Stepper(
-        currentStep: currentStep,
-        onStepTapped: (index) {
-          setState(() {
-            currentStep = index;
-          });
-        },
-        onStepContinue: () {
-          if (currentStep != 2) {
-            setState(() {
-              currentStep += 1;
-              _tsrName;            
-            });
-          }
-        },
-        onStepCancel: () {
-          if (currentStep != 0) {
-            setState(() {
-              currentStep -= 1;
-            });
-          }
-        },
-        controlsBuilder: (BuildContext context, ControlsDetails details) {
+        controlsBuilder: (BuildContext context, ControlsDetails controls) {
           return Padding(
-            padding: const EdgeInsets.only(top: 20.0),
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: details.onStepContinue,
-                  child: const Text('NEXT'),
-                ),
-                TextButton(
-                  onPressed: details.onStepCancel,
-                  child: const Text('BACK'),
-                ),
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                // ElevatedButton(
+                //   onPressed: controls.onStepContinue,
+                //   child: const Text('NEXT'),
+                // ),
+                if (_tsrName == null || _fileName == null)
+                  ElevatedButton(
+                    onPressed: controls.onStepContinue,
+                    child: _tsrName == null &&
+                            _fileName == null &&
+                            _currentStep == 2 &&
+                            _currentStep == 1
+                        ? const Text('BACK')
+                        : const Text('NEXT'),
+                  ),
+                //
+                if (_currentStep != 2 && _fileName != null && _tsrName != null)
+                  ElevatedButton(
+                    onPressed: controls.onStepContinue,
+                    child: const Text('NEXT..'),
+                  ),
+                //
+
+                if (_currentStep == 2 && _tsrName != null)
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const DataStats()),
+                      );
+                    },
+                    child: const Text('Load Stats'),
+                  ),
+                if (_currentStep == 1 || _fileName != null && _tsrName != null)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextButton(
+                      onPressed: controls.onStepCancel,
+                      child: const Text(
+                        'BACK',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  ),
               ],
             ),
           );
         },
-        steps: <Step>[
-          Step(
-            state: currentStep <= 0 ? StepState.editing : StepState.complete,
-            isActive: currentStep <= 0 ? false : true,
-            title: const Text('Choose CSV File:'),
-            content: GestureDetector(
-              onTap: () => _pickFile(),
-              child: Container(
-                decoration:  BoxDecoration(
-                    color: _fileName == null ? Colors.blueGrey :Colors.green,
-                    borderRadius: const BorderRadius.all(Radius.circular(5))),
-                height: 100,
-                child: Row(
-                  children: [
-                    _fileName == null
-                        ? Center(
-                            child: Row(
-                              children: const [
-                                Padding(
-                                  padding:
-                                      EdgeInsets.only(left: 20.0, right: 8.0),
-                                  child: Icon(
-                                    Icons.coronavirus_sharp,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Text(
-                                  'Tap to select a your file',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          )
-                        : Row(
-                            children: [
-                              const Padding(
-                                padding:
-                                    EdgeInsets.only(left: 20.0, right: 8.0),
-                                child: Icon(
-                                  Icons.tips_and_updates_sharp,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                _fileName.toString(),
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          )
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Step(
-            state: currentStep <= 1 ? StepState.editing : StepState.complete,
-            isActive: currentStep <= 1 ? false : true,
-            title: const Text('Choose your name:'),
-            content: Column(
-              children: [
-                DropdownButton(
-                  icon: const Icon(Icons.keyboard_arrow_down),
-                  items: _data.map((item) {
-                    return DropdownMenuItem(
-                      value: selectedItem,
-                      child: GestureDetector(
-                        child: Text(
-                          item[4].toString(),
-                          style: const TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        onTap: () {
-                          setState(() {
-                            _tsrName = item[4].toString();
-                          });
-                        },
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (String? val) {
-                    setState(() {
-                      selectedItem = _tsrName.toString();
-                    });
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Container(
-                    height: 100.0,
-                    decoration: BoxDecoration(
-                        color: _tsrName == null ? Colors.blueGrey : Colors.green,
-                        borderRadius: const BorderRadius.all(Radius.circular(5))),
-                    child: Center(
-                      child: Text(
-                        _tsrName.toString(),
-                        style: const TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // Step(
-          //   // state: currentStep <= 0 ? StepState.editing : StepState.complete,
-          //   isActive: currentStep <= 0 ? false : true,
-          //   state: StepState.complete,
-          //   // isActive: selectedItem == null || selectedItem == 'TSR' ? false : true, //Navigate to stats and warnings
-          //   title: const Text('Check your Warnings & Stats:'),
-          //   content: Container(color: Colors.amber),
-          // ),
-        ],
+        onStepTapped: (step) => setState(() => _currentStep = step),
+        onStepContinue: () {
+          setState(() {
+            if (_currentStep < _steps().length - 1) {
+              _currentStep += 1;
+            } else {
+              _currentStep = 0;
+            }
+          });
+        },
+        onStepCancel: () {
+          setState(() {
+            if (_currentStep > 0) {
+              _currentStep -= 1;
+            } else {
+              _currentStep = 0;
+            }
+          });
+        },
+        currentStep: _currentStep,
+        steps: _steps(),
       ),
     );
   }
@@ -285,72 +302,6 @@ class _MyHomePageState extends State<MyHomePage> {
         _fileName = checky;
         _data = fields;
       },
-    );
-  }
-}
-
-class _AddressForm extends StatelessWidget {
-  const _AddressForm({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Street',
-          ),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'City',
-          ),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Postcode',
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _CardForm extends StatelessWidget {
-  const _CardForm({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Card number',
-          ),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'Expiry date',
-          ),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: 'CVV',
-          ),
-        ),
-      ],
-    );
-  }
-}
-class _Overview extends StatelessWidget {
-  const _Overview({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Center(child: Text('Thank you for your order!')),
-      ],
     );
   }
 }
